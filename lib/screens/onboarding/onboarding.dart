4c69270a293be38037941427f7b86d671a0bf493
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-09-29 06:44:37 +0100

    added onboarding images

---
+++ lib/screens/onboarding/onboarding.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column()
+      ),
+    );
+  }
+}
